8737ef0a240626014073bad30786504095d59c6f
Dessoky <ahmeddessoky552001@gmail.com>
2020-12-27 18:41:45 +0200

    add all

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var username = "Dessoky";
+switch (username) {
+  case "Ahmed":
+    console.log("Welcome ya Ahmed");
+    break;
+  case "ali":
+    console.log("Welcome ya ali");
+    break;
+  case "mai":
+    console.log("Welcome ya mai");
+    break;
+    default :
+    console.log("Welcome ya user");
+    break;
+}
